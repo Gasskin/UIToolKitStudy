83a3baa07c1874a85b06b1b8324e1c445de1f091
Logarius <415753928@qq.com>
2022-03-12 17:51:48 +0800

    1

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,18 +1,47 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Random = System.Random;
 
 public class Test : MonoBehaviour
 {
     public UIDocument document;
-
-    private void Start ()
+    public VisualTreeAsset asset;
+    
+    private void Start()
     {
         var root = document.rootVisualElement;
-        var btn  = root.Q<Button> ("button1");
+        var scroll = root.Q<ListView>();
 
-        btn.clicked += (() =>
+        var items = new List<ListData>();
+        for (int i = 0; i < 10000; i++)
         {
-            Debug.Log ("点击");
-        });
+            items.Add(new ListData { name = $"新年快乐{i}", color = new Color(i/10000f,i/20000f,i/5000f, 1f) });
+        }
+
+        scroll.fixedItemHeight = 50f;
+        scroll.itemsSource = items;
+        scroll.makeItem = () => asset.CloneTree();
+        scroll.bindItem = (element, i) =>
+        {
+            element.Q<Label>().text = items[i].name;
+            element.Q<VisualElement>("img1").style.backgroundColor = items[i].color;
+        };
+        scroll.onSelectedIndicesChange += ints =>
+        {
+            foreach (var index in ints)
+            {
+                scroll.itemsSource[index] = new ListData { name = $"我被选中了{index}",color = Color.red};
+            }
+        };
+    }
+
+    class ListData
+    {
+        public string name;
+        public Color color;
     }
 }
